bd56fde42af99520185f7f6897d5ae5e34092799
Rishab-Sudhir <rishabsudhir2002@gmail.com>
2023-11-26 14:53:46 -0500

    Started working on saving sign up information

--- RestaurantTinder/authentication/views.py
+++ RestaurantTinder/authentication/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,6 +9,26 @@ def home(request):
     return render(request, "authentication/index.html")
 
 def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+    myuser = User.obejects.create_user(username, email, pass1)
+
+    myuser.first_name = fname
+    myuser.last_name = lname
+
+    myuser.save()
+
+    messages.success(request, "Your account has been successfully created!")
+
+    return redirect('signin')
+
     return render(request, "authentication/signup.html")
 
 def signin(request):
