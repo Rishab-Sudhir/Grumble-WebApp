5bd804f71396142e21164aa9cbea048b76df9780
Rishab-Sudhir <rishabsudhir2002@gmail.com>
2023-11-26 18:02:20 -0500

    Created function for emails to be sent to confirm user
    I forgot to push earlier, but I first created a function that sends a user a welcome email after they first create a profile. I am now working on creating a confirmation link.

---
+++ RestaurantTinder/RestaurantTinder/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True #security email will use
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'restauranttinderbu@gmail.com'
+EMAIL_HOST_PASSWORD = 'ptzrssgvsbkajliw' #APPS PASSWORD #Real is Testing@123
+EMAIL_PORT = 587
--- RestaurantTinder/authentication/views.py
+++ RestaurantTinder/authentication/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-
+from RestaurantTinder import settings
+from django.core.mail import EmailMessage, send_mail 
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from . tokens import generate_token
 
 # Create your views here.
 
@@ -20,20 +26,77 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
-        # Checking if both passwords match
-        if pass1 == pass2:
-            # If passwords match, continue with user creation
-            myuser = User.objects.create_user(username, email, pass1)
-            myuser.first_name = fname
-            myuser.last_name = lname
-            myuser.save()
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exists, please try another username!")
+            return redirect('home')
 
-            messages.success(request, "Your account has been successfully created!")
-            return redirect('signin')
-        else:
+
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered!")
+            return redirect('home')
+
+
+        if len(username) > 15:
+            messages.error(request, "Username must be under 15 characters!")
+            return redirect('home')
+
+        if not username.isalnum():
+            messages.error(request, "Username must be alphanumeric!")
+            return redirect('home')
+
+        # Checking if both passwords match
+        if pass1 != pass2:
+
             # If passwords do not match, return to the signup page with a message
             messages.error(request, "Passwords do not match!")
             return render(request, "authentication/signup.html")
+
+                
+
+        # If passwords match, continue with user creation
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.is_active = False
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created! We have sent you a confirmation email, please confirm your email in order to activate your account.")
+
+        #Welcome Email
+
+        subject = "Welcome to Tinder for Restaurants!"
+        message = "Hello " + myuser.first_name + "! \n Welcome to Tinder for Restaurants! \n Thank you for visting our website! \n We have also sent you a confirmation email, please confirm your email address in order to activate your account! \n Sincerely, \n The Team"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently = True)
+
+        #Confirmation Email
+
+        current_site = get_current_site(request)
+        email_subject = "Confirm your email @ Tinder for Restaurants"
+        message2 = render_to_string('email_confirmation.html',{
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser),
+        })
+
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
+
+
+
+
+
+
+        return redirect('signin')
+    
     else:
         return render(request, "authentication/signup.html")
 
