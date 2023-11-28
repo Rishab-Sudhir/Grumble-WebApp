b21e83e82fea6753c6ea5bf207d4a32921db0e9a
Rishab-Sudhir <rishabsudhir2002@gmail.com>
2023-11-28 17:11:31 -0500

    Updated get_Location function and inserted template location.html

--- RestaurantTinder/Location/views.py
+++ RestaurantTinder/Location/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def get_location(request):
     # logic to get the location or show location-related content
-    return render(request, 'location/get_location.html')
+    return render(request, 'Location/LocationMap.html')
