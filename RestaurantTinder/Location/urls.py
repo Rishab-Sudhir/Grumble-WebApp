8d68cbeb6b4e326ad422828db9ea7ecaabe2ae40
Rishab-Sudhir <rishabsudhir2002@gmail.com>
2023-11-28 18:43:23 -0500

    Updated L to l in apps.py

--- RestaurantTinder/Location/urls.py
+++ RestaurantTinder/Location/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('get_location/', views.get_location, name='get_location'),
+    path('search_restaurants/', views.search_restaurants, name='search_restaurants'),
 ]
