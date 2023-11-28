44b4da1dd068a72bddbd3fdafb7d78b9ed7e5dec
Rishab-Sudhir <rishabsudhir2002@gmail.com>
2023-11-28 18:17:06 -0500

    Got Locations Redirect to work
    Locations now runs after sign in

--- RestaurantTinder/Location/views.py
+++ RestaurantTinder/Location/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def get_location(request):
     # logic to get the location or show location-related content
     return render(request, 'Location/LocationMap.html')
+
+def search_restaurants(request):
+    pass
+#    if request.method == 'POST':
+#       # Extract data from form submission
+#        radius = request.POST.get('radius')
+#        location = request.POST.get('location')
+        
+        # You can now use the 'radius' and 'location' variables to make your API call and gather data
+
+        # After getting the data, you would typically render a results page
+#        return # render(request, 'Location/ResultsPage.html', {'data': data})
+#    else:
+        # If it's not a POST request, redirect back to the get_location view
+#        return # redirect('get_location')
