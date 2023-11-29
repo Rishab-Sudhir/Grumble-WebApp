ca74a704857ff17ab324f1f727f04a851e2d50fb
MahaAlali <maha9@bu.edu>
2023-11-29 15:30:48 -0500

    binary changes + deleted img folder
    title + a bunch of small changes + change in the the in the settings.html relating to the static files problem.

--- RestaurantTinder/RestaurantTinder/settings.py
+++ RestaurantTinder/RestaurantTinder/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "authentication",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -121,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
