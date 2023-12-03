db145f0905dd163434bb22c6afbb94ab761f1de6
MahaAlali <maha9@bu.edu>
2023-12-03 18:34:07 -0500

    changes to index.html design and map generating fixes

--- RestaurantTinder/static/js/location2.js
+++ RestaurantTinder/static/js/location2.js
@@ -1,12 +1,29 @@
-let map;
+let map, marker, geocoder;
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+function initMap() {
+    const defaultLocation = { lat: -34.397, lng: 150.644 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: defaultLocation,
+    });
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+    geocoder = new google.maps.Geocoder();
+
+    map.addListener('click', function(e) {
+        placeMarkerAndPanTo(e.latLng, map);
+    });
 }
 
-initMap();
+function placeMarkerAndPanTo(latLng, map) {
+    if (marker) {
+        marker.setPosition(latLng);
+    } else {
+        marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+        });
+    }
+
+    map.panTo(latLng);
+    geocodeLatLng(geocoder, latLng);
+}
