1c49d1efbb056c521ca7bf51fdde348535715422
MahaAlali <maha9@bu.edu>
2023-12-09 16:19:50 -0500

    fixes on js file for saved_restaurants
    work on delete button and minor css fixes

--- RestaurantTinder/static/js/saved.js
+++ RestaurantTinder/static/js/saved.js
@@ -1,44 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Handle detail view toggling
-    document.querySelectorAll('.details-button').forEach(button => {
-        button.addEventListener('click', function() {
-            const details = button.closest('.restaurant-item').querySelector('.restaurant-details');
-            details.style.display = details.style.display === 'none' ? 'block' : 'none';
-            button.textContent = details.style.display === 'none' ? 'More Details' : 'Less Details';
-        });
-    
-
-    // Handle delete functionality
-    const deleteButtons = document.querySelectorAll('.delete-button');
-    deleteButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const restaurantItem = button.closest('.restaurant-item');
-            const restaurantId = restaurantItem.getAttribute('data-id');
-            deleteRestaurant(restaurantId, restaurantItem);
-        });
-
-    restaurantItems.forEach(item => {
+    // Iterate over each restaurant item
+    document.querySelectorAll('.restaurant-item').forEach(item => {
         const detailsButton = item.querySelector('.details-button');
         const deleteButton = item.querySelector('.delete-button');
-        const details = item.querySelector('.restaurant-details');
 
         // Toggle details view
         detailsButton.addEventListener('click', function() {
+            const details = item.querySelector('.restaurant-details');
             details.style.display = details.style.display === 'none' ? 'block' : 'none';
+            detailsButton.textContent = details.style.display === 'none' ? 'More Details' : 'Less Details';
         });
 
         // Delete functionality
-        deleteButton.addEventListener('click', function(event) {
-            if (event.target.classList.contains('delete-button')) {
-                const yelpId = event.target.getAttribute('data-yelp-id');
-                deleteRestaurant(yelpId);
+        deleteButton.addEventListener('click', function() {
+            const yelpId = item.getAttribute('data-id');
+            if (confirm('Are you sure you want to delete this restaurant?')) {
+                deleteRestaurant(yelpId, item);
             }
         });
     });
 });
 
-
-function deleteRestaurant(yelpId) {
+function deleteRestaurant(yelpId, itemElement) {
     fetch('delete_saved_restaurant/', {
         method: 'POST',
         headers: {
@@ -51,15 +34,16 @@ function deleteRestaurant(yelpId) {
     .then(data => {
         if (data.status === 'success') {
             console.log('Restaurant deleted');
-            // Optionally remove the restaurant from the DOM
-            // document.querySelector(`[data-yelp-id="${yelpId}"]`).remove();
+            itemElement.remove();
         } else {
-            console.error('Error:', data.msg);
+            alert('Error: ' + data.msg);
         }
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred while deleting the restaurant.');
+    });
 }
-
 
 // Function to get CSRF token from cookies (required for POST requests)
 const getCookie = (name) => {
@@ -76,3 +60,4 @@ const getCookie = (name) => {
     }
     return cookieValue;
 };
+
