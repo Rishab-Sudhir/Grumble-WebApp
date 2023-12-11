8a08759468bb53d650ea57afd06a28edd6fbc33b
Rishab-Sudhir <rishabsudhir2002@gmail.com>
2023-12-11 11:17:39 -0500

    Changed Passwords for Email

--- RestaurantTinder/RestaurantTinder/info.py
+++ RestaurantTinder/RestaurantTinder/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True #security email will use
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'restauranttinderbu@gmail.com'
-EMAIL_HOST_PASSWORD = 'ptzrssgvsbkajliw' #APPS PASSWORD #Real is Testing@123
+EMAIL_HOST_PASSWORD = 'sencwdlrgocdsduq' #APPS PASSWORD 
 EMAIL_PORT = 587
